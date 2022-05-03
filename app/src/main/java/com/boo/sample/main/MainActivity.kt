7474bb6f158891d9920758b937af1a43b79982bc
Boo <Antwhale94@gmail.com>
2022-05-04 00:59:07 +0900

    just back up

--- app/src/main/java/com/boo/sample/main/MainActivity.kt
+++ app/src/main/java/com/boo/sample/main/MainActivity.kt
@@ -2,11 +2,25 @@ package com.boo.sample.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.boo.sample.repository.CommonRepository
+import com.boo.sample.repository.preference.PreferenceRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var preferenceRepository: PreferenceRepository
+
+    val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        preferenceRepository.setFirst(false)
+        val prefText = preferenceRepository.isFirst()
+        Log.d(TAG, "onCreate: $prefText")
     }
 }
